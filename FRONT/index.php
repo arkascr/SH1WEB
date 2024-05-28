0473162561cfb67baa025b5816540b23358946b1
Pablo Sena <pablo.sena@tagtico.com>
2024-05-27 21:48:51 -0600

    Ajuste de diseño

--- FRONT/index.php
+++ FRONT/index.php
@@ -122,26 +122,7 @@ $rCategorias = $libros->getCategorias();
                         </select>
                     </div>
                 </div>
-                <div class="row d-flex justify-content-center" id="tarjetas-contenedor">
-                    <?php
-                    $libros = new Libros();
-                    $result = $libros->getLibros();
-                    foreach ($result as $libro) :
-                        $imagen64 = base64_encode($libro["imagen"]);
-                    ?>
-
-                        <div class="col-lg-4 col-md-6 mb-4 d-flex justify-content-center">
-                            <div class="card bg-info-subtle" style="width: 300px;">
-                                <div class="card-body text-center px-4 py-5 px-md-5">
-                                    <a href="./producto.php?libro=<?= $libro["ID"] ?>"><img class="img-fluid d-block mx-auto" src="data:image/jpeg;base64, <?= $imagen64 ?>"></a>
-                                    <h5 class="fw-bold card-title mb-3"><?= htmlspecialchars($libro['titulo']); ?></h5>
-                                    <p class="fw-bold text-info card-text mb-2"><?= htmlspecialchars($libro['descripcion']); ?></p>
-                                    <p class="fw-bold text-info card-text mb-2"><?= htmlspecialchars('$' . $libro['precio']); ?></p>
-                                    <button class="btn btn-info btn-sm" type="button" onclick="location.href='./producto.php?libro=<?= $libro['ID'] ?>'">Leer</button>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
+                <div class="row d-flex justify-content-center" id="libroContainer">
                 </div>
             </div>
         </div>
@@ -231,7 +212,7 @@ $rCategorias = $libros->getCategorias();
                     type: 'GET',
                     data: { categoria: categoria },
                     success: function(response) {
-                        $('#tarjetas-contenedor').html(response);
+                        $('#libroContainer').html(response);
                     }
                 });
             }
--- FRONT/obtener_libros.php
+++ FRONT/obtener_libros.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../BACK/services/LibrosService.php');
 
-$categoria = isset($_GET['categoria']) ? $_GET['categoria'] : 'categoria1';
+$categoria = isset($_GET['categoria']) ? (int)$_GET['categoria'] : '0';
 
 $libros = new Libros();
 // Suponiendo que getLibros acepta un parámetro de categoría
@@ -13,11 +13,10 @@ foreach ($result as $libro) :
     <div class="col-lg-4 col-md-6 mb-4 d-flex justify-content-center">
         <div class="card bg-info-subtle" style="width: 300px;">
             <div class="card-body text-center px-4 py-5 px-md-5">
-                <a href="./producto.php?libro=<?= $libro["ID"] ?>"><img class="img-fluid d-block mx-auto" src="data:image/jpeg;base64, <?= $imagen64 ?>"></a>
-                <h5 class="fw-bold card-title mb-3"><?= htmlspecialchars($libro['titulo']); ?></h5>
-                <p class="fw-bold text-info card-text mb-2"><?= htmlspecialchars($libro['descripcion']); ?></p>
-                <p class="fw-bold text-info card-text mb-2"><?= htmlspecialchars('$' . $libro['precio']); ?></p>
-                <button class="btn btn-info btn-sm" type="button" onclick="location.href='./producto.php?libro=<?= $libro['ID'] ?>'">Leer</button>
+				<h5 class="fw-bold card-title mb-3"><?= htmlspecialchars($libro['titulo']); ?></h5>
+                <a href="./producto.php?libro=<?= $libro["ID"] ?>"><img width="200" height="250" class="d-block mx-auto" src="data:image/jpeg;base64, <?= $imagen64 ?>" title="<?= $libro['titulo'] ?>"></a>
+                <br>
+                <button class="btn btn-info btn-sm" type="button" onclick="location.href='./producto.php?libro=<?= $libro['ID'] ?>'">Leer libro</button>
             </div>
         </div>
     </div>
